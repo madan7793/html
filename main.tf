7ab59197f31db9397937cf12270b16789e0d3d52
madan7793 <madanmohan7793@gmail.com>
2024-06-24 15:14:31 +0000

    tesing github actions

--- main.tf
+++ main.tf
@@ -2,49 +2,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_msk_cluster" "kafka_cluster" {
-  cluster_name           = "example-cluster"
-  kafka_version          = "2.8.1"
-  number_of_broker_nodes = 2
-
-  broker_node_group_info {
-    instance_type   = "kafka.m5.large"
-    client_subnets  = ["subnet-03151fd3b6eaba29d", "subnet-00558958d15ffec4b"]
-    security_groups = ["sg-0149ec6f0649ba6fe"]
-  }
-
-  encryption_info {
-    encryption_in_transit {
-      client_broker = "TLS"
-      in_cluster    = true
-    }
-  }
-
-  tags = {
-    Name = "example-kafka-cluster"
-  }
+module "vpc" {
+  source              = "./terraform/modules/vpc"
+  cidr_block          = "10.0.0.0/16"
+  name                = "example-vpc"
+  subnet_cidr_blocks  = ["10.0.1.0/24", "10.0.2.0/24"]
+  availability_zones  = ["us-east-1a", "us-east-1b"]
 }
 
-resource "aws_security_group" "kafka_sg" {
-  name        = "kafka_sg"
-  description = "Security group for MSK cluster"
-
-  ingress {
-    from_port   = 9092
-    to_port     = 9092
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+module "eks" {
+  source            = "./terraform/modules/eks"
+  cluster_name      = "example-cluster"
+  cluster_role_arn  = aws_iam_role.cluster.arn
+  node_role_arn     = aws_iam_role.node.arn
+  subnet_ids        = module.vpc.subnet_ids
+  node_group_name   = "example-node-group"
+  instance_types    = ["t3.medium"]
+  desired_size      = 2
+  max_size          = 3
+  min_size          = 1
 }
 
-output "bootstrap_brokers" {
-  value = aws_msk_cluster.kafka_cluster.bootstrap_brokers
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "kubeconfig" {
+  value = module.eks.kubeconfig
 }
 
