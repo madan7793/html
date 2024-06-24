2f98279ad0d98f39cb067d0bdb8e346addd6cd21
madan7793 <madanmohan7793@gmail.com>
2024-06-24 15:51:07 +0000

    tesing github actions

--- main.tf
+++ main.tf
@@ -23,15 +23,6 @@ module "eks" {
   min_size          = 1
 }
 
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig
-}
 
