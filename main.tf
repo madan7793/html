5d7ed0b65ee79e75dd6b0e6a0fc6190dbc222610
madan7793 <madanmohan7793@gmail.com>
2024-06-28 11:58:34 +0000

    tesing github actions

--- main.tf
+++ main.tf
@@ -1,28 +1,49 @@
+# main.tf
 provider "aws" {
   region = "us-east-1"
 }
 
-module "vpc" {
-  source              = "./terraform/modules/vpc"
-  cidr_block          = "10.0.0.0/16"
-  name                = "example-vpc"
-  subnet_cidr_blocks  = ["10.0.1.0/24", "10.0.2.0/24"]
-  availability_zones  = ["us-east-1a", "us-east-1b"]
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.eks_subnets[*].id
+  }
 }
 
-module "eks" {
-  source            = "./terraform/modules/eks"
-  cluster_name      = "example-cluster"
-  cluster_role_arn  = aws_iam_role.cluster.arn
-  node_role_arn     = aws_iam_role.node.arn
-  subnet_ids        = module.vpc.subnet_ids
-  node_group_name   = "example-node-group"
-  instance_types    = ["t3.medium"]
-  desired_size      = 2
-  max_size          = 3
-  min_size          = 1
+resource "aws_iam_role" "eks_role" {
+  name = "eks-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ],
+  })
 }
 
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
 
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
 
+resource "aws_subnet" "eks_subnets" {
+  count             = 2
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+}
+
+data "aws_availability_zones" "available" {}
 
